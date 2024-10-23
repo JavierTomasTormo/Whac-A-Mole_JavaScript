8232f0774e50d56b5cb93df25fc40d9b4af2998e
JavierTomasTormo <106741000+JavierTomasTormo@users.noreply.github.com>
2024-10-23 19:55:15 +0200

    Wallappers && pantallaCarga.css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
     const images = [
+            'assets/images/Wallpaper_Charge/wv9.png',
         'assets/images/Wallpaper_Charge/wv1.png',
+            'assets/images/Wallpaper_Charge/wv8.png',
         'assets/images/Wallpaper_Charge/wv2.png',
+            'assets/images/Wallpaper_Charge/wv7.png',
         'assets/images/Wallpaper_Charge/wv3.png',
+            'assets/images/Wallpaper_Charge/wv6.png',
         'assets/images/Wallpaper_Charge/wv4.png',
+
     ];
     const randomImage = images[Math.floor(Math.random() * images.length)];
     document.getElementById('start-screen').style.backgroundImage = `url('${randomImage}')`;
